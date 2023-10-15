40f57176c3a1b409f4df1d472bfe9c79ef94a230
J. Romero <JRomero.yyc@gmail.com>
2023-10-15 13:37:11 -0400

    map view working on ios

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,37 +1,33 @@
 import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+const MyMap = () => (
+  <MapView
+    style={{flex: 1}}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
+);
 
-const Map = () => {
-  return (
-  <LoadScript googleMapsApiKey="AIzaSyDBXgyLmpbdrVB2tzfGrrzqt1bTnbkWuww">
-    <GoogleMap
-      mapContainerStyle={{ width: "100%", height: "400px" }}
-      center={{ lat: 37.7749, lng: -122.4194 }} // Set your initial center coordinates
-      zoom={8} // Set your initial zoom level
-    ></GoogleMap>
-  </LoadScript>
-  );
-};
+export default MyMap;
 
-
-export default Map;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//   },
-//   separator: {
-//     marginVertical: 30,
-//     height: 1,
-//     width: '80%',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
