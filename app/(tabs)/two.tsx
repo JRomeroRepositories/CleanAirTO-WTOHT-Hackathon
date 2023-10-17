3deca6ce4eaa339c005e95059611d95dc93d0fc1
J. Romero <JRomero.yyc@gmail.com>
2023-10-17 07:37:14 -0400

    Added Map markers
    temp and AQI

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,19 +1,39 @@
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-const MyMap = () => (
-  <MapView
-    style={{flex: 1}}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-  />
-);
+const MyMap = () => {
+  const locationData = [
+    { latitude: 43.64499649047999, longitude: -79.39429402110466, aqi: 1.2, temperature: 22 },
+    { latitude: 43.63499649047999, longitude: -79.38429402110466, aqi: 2.3, temperature: 24 },
+    { latitude: 43.62499649047999, longitude: -79.37429402110466, aqi: 2.9, temperature: 20 },
+    // ... add more locations, AQI values, and temperatures as needed
+  ];
 
-export default MyMap;
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 43.64499649047999,
+        longitude: -79.39429402110466,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+      {locationData.map((data, index) => (
+        <Marker
+          key={index}
+          coordinate={{ latitude: data.latitude, longitude: data.longitude }}
+        >
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataText}>AQI: {data.aqi}</Text>
+            <Text style={styles.dataText}>Temp: {data.temperature}°C</Text>
+          </View>
+        </Marker>
+      ))}
+    </MapView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -30,4 +50,52 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  dataContainer: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderRadius: 5,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  dataText: {
+    fontWeight: 'bold',
+  },
 });
+
+export default MyMap;
+
+
+
+// import { StyleSheet } from 'react-native';
+// import MapView from 'react-native-maps';
+
+// const MyMap = () => (
+//   <MapView
+//     style={{flex: 1}}
+//     initialRegion={{
+//       latitude: 43.64499649047999,
+//       longitude: -79.39429402110466,
+//       latitudeDelta: 0.0922,
+//       longitudeDelta: 0.0421,
+//     }}
+//   />
+// );
+
+// export default MyMap;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },\
+//});
